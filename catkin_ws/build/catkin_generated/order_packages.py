c0d7aad0b6bce918e115c9df0a64356ccb07a229
dalek669528 <dalek669528@gmail.com>
2020-09-13 15:27:40 +0800

    catkin package

--- catkin_ws/build/catkin_generated/order_packages.py
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/sdjl/SDJL_robot_combat/catkin_ws/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/opt/ros/melodic'.split(';') if '/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/sdjl/SDJL_robot_combat/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/sdjl/SDJL_robot_combat/catkin_ws/devel;/opt/ros/melodic' != '' else []
--- catkin_ws/build/cmake_install.cmake
+++ catkin_ws/build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/sdjl/SDJL_robot_combat/catkin_ws/build/gtest/cmake_install.cmake")
+  include("/home/sdjl/SDJL_robot_combat/catkin_ws/build/motor_driver/cmake_install.cmake")
 
 endif()
 
