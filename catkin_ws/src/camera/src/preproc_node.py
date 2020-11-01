2a05ff7c4b590585f079f258283c0f1c5772afa2
luca610980 <luca610980@gmail.com>
2020-11-01 21:45:37 +0800

    little bug fixed

--- catkin_ws/src/camera/src/preproc_node.py
+++ catkin_ws/src/camera/src/preproc_node.py
@@ -6,7 +6,7 @@ import numpy as np
 import pyrealsense2 as rs
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-from std_msgs.msg import Int8
+from std_msgs.msg import Int32, Int32MultiArray
 from detectROI import detect_color
 from crop_ROI  import crop_depth
 from calculateXY import calculate_coordinate
@@ -28,8 +28,8 @@ class Preprocess(object):
         self.RemoveBG = rospy.Subscriber('RemoveBG', Image, self.RemoveBG_callback, queue_size=1)
 
         # Publishers
-        self.Max_color = rospy.Publisher('MaxColor', Int32, queue_size=1) # publish max detected color
-        self.Max_coord = rospy.Publisher('MaxCoord', Int32MultiArray, queue_size=1) # publish coordination of max color
+        #self.Max_color = rospy.Publisher('MaxColor', Int32, queue_size=1) # publish max detected color
+        #self.Max_coord = rospy.Publisher('MaxCoord', Int32MultiArray, queue_size=1) # publish coordination of max color
         #self.Publisher_RGB = rospy.Publisher('CropedRGB', Image, queue_size=1) # RGB publish
         #self.CroppedRBG = rospy.Publisher('CropedRGB', Image, queue_size=1) # Depth publish
         #self. = rospy.Publisher('', Image, queue_size=1) # Removed BG publish
@@ -44,12 +44,12 @@ class Preprocess(object):
         max_color = -1
         max_roi = np.array([1, 4])
         detected_image, roi_array, roi_count = detect_color(self.removeBG_image, 0.3)
+        cv2.imshow('Detected image', detected_image)
+        cv2.waitKey(1)
 
         if roi_array.shape[0] == 0: # Nothing detected
             print('Nothing')
             return 0
-        cv2.imshow('Detected image', detected_image)
-        cv2.waitKey(1)
 
         x1 = roi_array[:, 0]
         y1 = roi_array[:, 1]
@@ -67,16 +67,16 @@ class Preprocess(object):
             max_color = 2
         print(max_color)
         max_roi = roi_array[max_index, :].reshape([1, 4])
-        depth_img_array = crop_depth(max_roi, self.depth_image) # return detected (x, y, depth)
+        depth_img_array = crop_depth(max_roi, self.depth_image) # return detected array(x, y, depth)
         #depth_img_array = crop_depth(roi_array, self.depth_image) # return detected (x, y, depth)
         coordinate_array = calculate_coordinate(depth_img_array)
-        print(coordinate_array) # (x, y(depth), h)
+        print(coordinate_array) # array(x, y(depth), h)
 
         # publish
         #msg_RGB = CvBridge().cv2_to_imgmsg(flipped_image)
         #self.Publisher_RGB.publish(msg_RGB)
-        self.Max_color.publish(max_color)
-        self.Max_coord.publish(max_roi)
+        #self.Max_color.publish(max_color)
+        #self.Max_coord.publish(max_roi)
 
     def Depth_callback(self, data):
         try:
